839603050a80bffd2e232007c8cacae4509e58f6
ELENA ADAN LLEDO <126772898+Elenaa72@users.noreply.github.com>
2025-06-26 17:22:30 +0200

    Update 3_Clasificación por conflictividad.py

--- app/pages/3_Clasificación por conflictividad.py
+++ app/pages/3_Clasificación por conflictividad.py
@@ -1,6 +1,6 @@
-##import pandas as pd
+import pandas as pd
 #import numpy as np
-#import streamlit as st
+import streamlit as st
 #from sklearn.ensemble import RandomForestClassifier
 #from sklearn.model_selection import train_test_split
 #from sklearn.metrics import classification_report, accuracy_score
