2b2ea703426dcb89897f6616cae95945fd3380af
ELENA ADAN LLEDO <126772898+Elenaa72@users.noreply.github.com>
2025-06-26 17:21:58 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
-#import pandas as pd
+import pandas as pd
 #import matplotlib.pyplot as plt
 #import seaborn as sns
 #import folium
 #from folium import GeoJsonTooltip
 #from streamlit_folium import st_folium
 #from sklearn.cluster import KMeans
-#import streamlit as st
+import streamlit as st
 #import json
 
 st.set_page_config(layout="wide")
