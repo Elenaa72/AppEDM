27be78477d52a72c12cdc87e043d98ad3fcdc0d1
ELENA ADAN LLEDO <126772898+Elenaa72@users.noreply.github.com>
2025-06-26 17:18:02 +0200

    Update 3_Clasificación por conflictividad.py

--- app/pages/3_Clasificación por conflictividad.py
+++ app/pages/3_Clasificación por conflictividad.py
@@ -1,10 +1,10 @@
-import pandas as pd
-import numpy as np
-import streamlit as st
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, accuracy_score
-import plotly.express as px
+##import pandas as pd
+#import numpy as np
+#import streamlit as st
+#from sklearn.ensemble import RandomForestClassifier
+#from sklearn.model_selection import train_test_split
+#from sklearn.metrics import classification_report, accuracy_score
+#import plotly.express as px
 
 # ----------- FUNCIONES -----------
 
